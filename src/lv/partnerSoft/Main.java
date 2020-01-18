2774dd82dfe120eab7936ab0891a54eb7f07abc9
Oleg Miroshnikov <oleg.psoft@gmail.com>
2020-01-18 21:24:20 +0200

    First update (amend)

--- src/lv/partnerSoft/Main.java
+++ src/lv/partnerSoft/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println ("That is my application");
+        System.out.println ("That is first by first update");
     }
 }
