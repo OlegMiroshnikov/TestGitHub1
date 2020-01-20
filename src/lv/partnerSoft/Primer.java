c0cced0bcd22990d838a0dbcb0fdb9f01f0645ec
Oleg Miroshnikov <oleg.psoft@gmail.com>
2020-01-20 21:03:41 +0200

    Update in master branch

--- src/lv/partnerSoft/Primer.java
+++ src/lv/partnerSoft/Primer.java
@@ -3,5 +3,6 @@ package lv.partnerSoft;
 public class Primer {
     void toPrint() {
         System.out.println ("That is print class");
+        System.out.println ("That are changes in the master branch");
     }
 }
